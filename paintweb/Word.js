d06a98381318d7397c09da872b620e635b0e88c2
tdjneate <tdjneate@gmail.com>
2019-11-26 12:41:44 +0000

    testFile added

---
+++ paintweb/Word.js
@@ -0,0 +1,20 @@
+class Word
+{
+    constructor (txt, xPos, yPos)
+    {
+        this.txt = txt;
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+
+    function setxPos(xPos)
+    {
+        this.xPos = xPos;
+    }
+   
+    function setyPos(yPos)
+    {
+        this.yPos = yPos;
+    }
+
+}
