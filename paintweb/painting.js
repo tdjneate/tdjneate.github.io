21978a8bed7224bf5fad8087993c57f7b6da4805
tdjneate <tdjneate@gmail.com>
2020-02-14 17:40:39 +0000

    mirror offset bug alteration

--- paintweb/painting.js
+++ paintweb/painting.js
@@ -323,7 +323,7 @@ function modeSelector()
   }
 }
 else{
-      var  mirrorPoint = [(window.innerWidth/2) / 1.6, (window.innerHeight/2) / 1.4]; // mirroroffset is currently 0 - 1. Could be weighted otherwise
+      var  mirrorPoint = [(window.innerWidth * .6) / 2 , (window.innerHeight * .4) / 2)]; // mirroroffset is currently 0 - 1. Could be weighted otherwise
     
       
       var points  =  [mouseX, mouseY];
