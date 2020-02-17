7e0d75c43f32b9353e13a10adebc1101b262790f
Timothy Neate <tdjneate@timothys-mbp.home>
2020-02-17 17:08:38 +0000

    added test pics

--- paintweb/painting.js
+++ paintweb/painting.js
@@ -224,7 +224,7 @@ function loadImageAndUpdatePreview()
 {
     img = loadImage("pics/" + imgs[imageLoaded] + ".jpg");
     
-   img.resize(innerWidth/2, innerWidth/3;
+   img.resize(innerWidth/2, innerWidth/3);
     
     //updatePreview
     var previewPicture = select('#previewPicture');
